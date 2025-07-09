0f10be780fc74312cf34c9b43760baed5d9f1624
jeromecagado <jerome.cagado@outlook.com>
2025-07-09 12:38:25 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment1._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars;
+            int[] myArray = new int[3];
+            string[] germanCars = new string[3] { "Audi", "Mercedes", "BMW" };
+            string[] footballTeams = new string[] { "Liverpool, Chelsea, Arsenal, Man City" };
+            int[] numArray = { 1, 2, 3, };
+        }
+    }
+}
