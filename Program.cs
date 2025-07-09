04bf7df1973b3f47dfee2f1dc5f7c799662faed5
jeromecagado <jerome.cagado@outlook.com>
2025-07-09 13:25:28 -0700

    Add new things.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@
             string[] cars;
             int[] myArray = new int[3];
             string[] germanCars = new string[3] { "Audi", "Mercedes", "BMW" };
+            Console.WriteLine(germanCars[0]);
             string[] footballTeams = new string[] { "Liverpool, Chelsea, Arsenal, Man City" };
+            Console.WriteLine(footballTeams[3]);
             int[] numArray = { 1, 2, 3, };
         }
     }
